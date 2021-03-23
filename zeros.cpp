38abb35bd6b720e7792c1618999f15832f246946
Tushar Mehta <56684401+Tushar00728@users.noreply.github.com>
2021-03-23 20:43:49 +0530

    Update zeros.cpp

--- zeros.cpp
+++ zeros.cpp
@@ -57,7 +57,7 @@ void sortArr(int arr[], int n)
 int main() 
 { 
     int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 }; 
-    int n = sizeof(arr) / sizeof(int); 
+    int n = sizeof(arr) / sizeof(arr[0); 
   
     sortArr(arr, n); 
   

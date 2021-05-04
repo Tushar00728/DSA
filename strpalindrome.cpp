7aaedeecdd532f111ffba01cb2e07b540fb6de26
Tushar Mehta <56684401+Tushar00728@users.noreply.github.com>
2021-05-04 21:19:17 +0530

    Add files via upload

---
+++ strpalindrome.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void checkPalindrome(char x[])
+{
+
+    int i = 0;
+    int j = strlen(x)-1;
+    
+    while(j>i)
+    {
+        if(x[i++] != x[j--] )
+        {
+            cout<<"Not a palindrome"<<endl;
+            return ;
+        }
+
+
+    }
+
+    cout<<"Given string is a palindrome";
+}
+
+
+int main()
+{
+
+   checkPalindrome("abba");
+   
+}
